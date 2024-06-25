be4869b12cdcd681dfeca29929df220dfa9b2f18
popyson <popyson1648@gmail.com>
2024-06-25 21:36:47 +0900

    .

--- xpost.py
+++ xpost.py
@@ -2,7 +2,6 @@ import os
 import requests
 import json
 import re
-from typing import Dict, Union, List
 
 # Medium APIトークン
 token = os.getenv('MEDIUM_API_TOKEN')
@@ -18,21 +17,11 @@ headers = {
 
 def get_user_id() -> str:
     response = requests.get(f'{base_url}/me', headers=headers)
-    try:
-        response.raise_for_status()  # HTTPエラーチェック
-        user_info = response.json()
-        print(user_info)  # デバッグ用にレスポンス全体を表示
-        return user_info['data']['id']
-    except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except requests.exceptions.RequestException as err:
-        print(f'Error occurred: {err}')
-    except json.JSONDecodeError:
-        print('Error decoding JSON response')
-        print(f'Response content: {response.content}')  # レスポンスの内容を表示
-    return ''  # エラー時は空文字を返す
+    user_info = response.json()
+    print(user_info['data']['id'])
+    return user_info['data']['id']
 
-def parse_metadata(content: str) -> Dict[str, str]:
+def parse_metadata(content: str) -> dict[str, str]:
     metadata = {}
     match = re.search(r'<!--(.*?)-->', content, re.DOTALL)
     if match:
@@ -43,30 +32,16 @@ def parse_metadata(content: str) -> Dict[str, str]:
                 metadata[key.strip()] = value.strip()
     return metadata
 
-def read_event_data() -> Dict:
+def read_event_data() -> dict:
     event_path = os.getenv('GITHUB_EVENT_PATH')
     with open(event_path, 'r') as f:
         return json.load(f)
 
-def post_to_medium(user_id: str, post_data: Dict[str, Union[str, List[str], bool]]) -> None:
+def post_to_medium(user_id: str, post_data: dict[str, str | list[str] | bool]) -> None:
     response = requests.post(f'{base_url}/users/{user_id}/posts', headers=headers, json=post_data)
-    try:
-        response.raise_for_status()  # HTTPエラーチェック
-        print(f'Successfully posted to Medium: {response.json()}')
-    except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except requests.exceptions.RequestException as err:
-        print(f'Error occurred: {err}')
-    except json.JSONDecodeError:
-        print('Error decoding JSON response')
-        print(f'Response content: {response.content}')  # レスポンスの内容を表示
 
 def main() -> None:
     user_id = get_user_id()
-    if not user_id:
-        print("Failed to retrieve user ID. Exiting...")
-        return
-
     event_data = read_event_data()
 
     for commit in event_data['commits']:
